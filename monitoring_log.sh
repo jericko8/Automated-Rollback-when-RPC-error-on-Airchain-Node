fe5392b347a16cb8cba4c118756d1a2cedb47c8b
jericko8 <dimasarianto68@gmail.com>
2024-07-05 03:08:22 +0800

    menambahkan kode untuk mengatasi invalid pod

--- monitoring_log.sh
+++ monitoring_log.sh
@@ -39,6 +39,7 @@ error_count=0
 
 
 #fungsi buat nambahin tulisan
+
 display_message() {
     local blue='\033[0;34m'      
     local NC='\033[0m'           
@@ -68,43 +69,65 @@ display_message() {
 # Memeriksa log secara terus-menerus
 #echo -e "${BLUE}Monitoring Logs for Error msg..${NC}"
 
-display_message
+display_message 
+display_message 
 sudo journalctl -u stationd -f --no-hostname -o cat | while IFS= read -r line
 do
     # Memeriksa jika baris mengandung kata eror
     if echo "$line" | grep -iq "error"; then
         ((error_count++))
-        # Jika ditemukan lebih dari satu eror
-        if [ "$error_count" -gt 4 ]; then
-            echo -e "${RED}Error berhasil ditemukan...${NC}"
-            echo -e "${GREEN}Masuk ke HOME directory${NC}"
-            cd $HOME
-            sleep 3
-            echo -e "${YELLOW}Memberhentikan Stationd${NC}"
-            systemctl stop stationd &
-            spinner
-            spinner
-            sleep 120
-            echo -e "${GREEN}StationD Telah berhenti${NC}"
-            sleep 3
-            echo "${GREEN}Memeriksa Pembaruan....."
-            cd tracks && git pull 
-            sleep 3
-            echo "${GREEN}Pembaruan Telah Berhasil..."
-            sleep 2
-            echo -e "${YELLOW}Memulai Rollback${NC}"
-            run_rollback
-            sleep 3
-            echo -e "${GREEN}Rollback Berhasil dilakukan${NC}"
-            sleep 3
-            echo -e "${YELLOW}Memulai kembali StationD${NC}"
-            systemctl restart stationd &
-            spinner
-            echo -e "${GREEN}StationD berhasil di Restart${NC}"
-            display_message
-            #echo -e "${BLUE}Monitoring Logs for Error msg..${NC}"
-            # Reset penghitung eror setelah menjalankan langkah-langkah
-            error_count=0
-        fi
+    fi
+    
+    # Memeriksa jika baris mengandung pesan kesalahan tertentu
+    if echo "$line" | grep -q "» Failed to Transact Verify pod"; then
+        echo -e "${RED}Masalah ditemukan: Failed to Transact Verify pod${NC}"
+        sleep 3
+        echo -e "${YELLOW}Memulai kembali StationD${NC}"
+        sleep 3
+        systemctl restart stationd 
+        sleep 3
+        echo -e "${GREEN}StationD berhasil di Restart${NC}"
+    fi
+
+    # if echo "$line" | grep -q "» Failed to get transaction by hash: not found"; then
+    #     echo -e "${RED}Masalah ditemukan: Failed to get transaction by hash: not found${NC}"
+    #     sleep 3
+    #     echo -e "${YELLOW}Memulai kembali StationD${NC}"
+    #     sleep 3
+    #     systemctl restart stationd 
+    #     sleep 3
+    #     echo -e "${GREEN}StationD berhasil di Restart${NC}"
+    #     # Tambahkan tindakan yang diinginkan di sini, jika ada
+    # fi
+
+    # Jika ditemukan lebih dari empat eror
+    if [ "$error_count" -gt 4 ]; then
+        echo -e "${RED}Error berhasil ditemukan...${NC}"
+        echo -e "${GREEN}Masuk ke HOME directory${NC}"
+        cd $HOME
+        sleep 3
+        echo -e "${YELLOW}Memberhentikan Stationd${NC}"
+        systemctl stop stationd &
+        spinner
+        sleep 120
+        echo -e "${GREEN}StationD Telah berhenti${NC}"
+        sleep 3
+        echo "${GREEN}Memeriksa Pembaruan....."
+        cd tracks && git pull 
+        sleep 3
+        echo "${GREEN}Pembaruan Telah Berhasil..."
+        sleep 2
+        echo -e "${YELLOW}Memulai Rollback${NC}"
+        run_rollback
+        sleep 3
+        echo -e "${GREEN}Rollback Berhasil dilakukan${NC}"
+        sleep 3
+        echo -e "${YELLOW}Memulai kembali StationD${NC}"
+        systemctl restart stationd &
+        spinner
+        echo -e "${GREEN}StationD berhasil di Restart${NC}"
+        display_message
+        # Reset penghitung eror setelah menjalankan langkah-langkah
+        error_count=0
     fi
 done
